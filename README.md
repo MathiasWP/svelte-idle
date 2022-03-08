d462176f658430749f6d95b9620d8301fcd8d7a5
Mathias Picker <mattis123@live.no>
2022-03-08 23:35:19 +0100

    init ✨

---
+++ README.md
@@ -0,0 +1,83 @@
+<p align="center">
+    <img width="100%" src="./media/banner.png" />
+</p>
+
+<br />
+
+<h2 align="center">
+Features | <a href="link-to-demo">Demo</a>
+</h2>
+
+<p align="center">
+    SSR support &mdash; Readable store for idle value &mdash; `onIdle` callback
+<p align="center">
+
+<br/>
+
+<h2 align="center">
+Usage
+</h2>
+
+<br />
+
+```svelte
+<script>
+import { start, idle, onIdle } from 'svelte-idle'
+
+// Run start on component Initialization
+start()
+
+// Run code when user idle via callback...
+onIdle(() => {
+    console.log('User is idle')
+})
+
+//... or by using the idle store
+$: {
+    if($idle) console.log('User is idle')
+
+}
+</script>
+
+User is idle: {$idle}
+```
+<br />
+
+<h2 align="center">
+API
+</h2>
+
+## start
+The start method accepts an optional object (type: `StartOpts`). The following values can be defined:
+
+### milliseconds
+- type: `number`
+- defines: how long before the user is udle
+
+### throttle_milliseconds
+- type: `number``
+- defines: amount of milliseconds before each idle-check
+
+
+#### Example:
+```ts
+import { start } from 'svelte-idle'
+
+start({
+    milliseconds: 60_000,
+    throttle_milliseconds: 500
+})
+```
+
+## idle
+A readable store that reflects the current idle-state.
+
+## onIdle
+Callback which will be fired everytime idle becomes true.
+
+#### Example:
+```ts
+import { onIdle } from 'svelte-idle'
+
+onIdle(() => console.log('User is idle!'))
+```
---
+++ src/index.ts
@@ -0,0 +1,109 @@
+import { onMount } from "svelte";
+import { readable } from "svelte/store";
+import type { Subscriber } from "svelte/store";
+
+/**
+ * Public stuff
+ */
+export let idle = readable(false, (set) => (update_store = set) && (() => set(false)));
+
+export type StartOpts = {
+    milliseconds?: number,
+    throttle_milliseconds?: number
+}
+
+export function start(opts: StartOpts = {}) {
+    if(!IS_BROWSER) return;
+
+    if(typeof opts.milliseconds === 'number' && opts.milliseconds > 0) idle_timeout_ms = opts.milliseconds;
+    if(typeof opts.throttle_milliseconds === 'number' && opts.throttle_milliseconds > 0) throttle_timeout_ms = opts.throttle_milliseconds;
+
+    onMount(watch)
+}
+
+export function onIdle(cb: () => any) {
+    if(!IS_BROWSER) return;
+
+    if(!idle_callbacks.has(cb)) idle_callbacks.add(cb)
+    return () => idle_callbacks.delete(cb)
+}
+
+/**
+ * Private stuff
+ */
+const idle_callbacks: Set<() => any> = new Set()
+
+let watchers = 0;
+let is_throttling = false;
+let update_store: Subscriber<boolean>;
+let is_idle = false;
+let idle_countdown: /* Timeout */ any;
+let idle_timeout_ms = 1_000 * 60 * 10; // Default is 10 minutes
+let throttle_timeout_ms = 200;
+
+const IS_BROWSER = !!(window && document);
+const INTERESTING_EVENTS = [
+    "keypress",
+    "keydown",
+    "click",
+    "contextmenu",
+    "dblclick",
+    "mousemove",
+    "scroll",
+    "touchmove",
+    "touchstart"
+]
+
+function watch() {
+    
+    if(watchers > 0) return;
+    watchers++;
+
+    // keeping track of store value for easy access
+    const unsubscribe = idle.subscribe(i => is_idle = i)
+
+    start_countdown()
+
+    for(const event of INTERESTING_EVENTS) {
+        document.addEventListener(event, detect_action, { passive: true })
+    }
+
+    return () => {
+        watchers--;
+
+        if(watchers === 0) {
+            for(const event of INTERESTING_EVENTS) {
+                document.removeEventListener(event, detect_action)
+            }
+            clear_countdown()
+            unsubscribe()
+        }
+    }
+}
+
+function start_countdown() {
+    idle_countdown = setTimeout(() => {
+        update_store(true)
+        idle_callbacks.forEach(fn => fn())
+    }, idle_timeout_ms);
+}
+
+function clear_countdown() {
+    if(idle_countdown) clearTimeout(idle_countdown)
+}
+
+function start_throttle() {
+    setTimeout(() => is_throttling = false, throttle_timeout_ms);
+}
+
+function detect_action() {
+    if(is_throttling) return
+    if(is_idle) update_store(false)
+
+    is_throttling = true;
+
+    clear_countdown()
+    start_countdown()
+
+    start_throttle()
+}
